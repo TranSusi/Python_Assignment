47d2e0095cb71a93db43cb6799185f9c7fc620b3
Linh Tran <ttplinh16@gmail.com>
2021-08-28 14:22:23 +0300

    Try Loop

--- A1.py
+++ A1.py
@@ -1,3 +1,23 @@
-astring = "Hello world!"
-print(astring[3:7])
-print(astring[3:7:1])
+# Using Loop, 'for..in'
+
+# Using Xrange to iterate over a sequence of numbers
+for y in range(6):
+    print(y) #Prints out 0,1,2,3,4
+    
+for y in range(4,7):
+    print(y) #prints put (4,5,6)
+
+    
+for x in range(4,10,2):
+    print(x) #prints out 4,6,8
+
+#Using 'While' in Loop with boolean condition
+
+# generating numbers from 0 to 10 with +-1
+# Prints out 0,1,2,3,4
+
+count = 0
+while count < 6:
+    print(count)
+    count = count + 1 
+   
